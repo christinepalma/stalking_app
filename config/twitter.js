dd5dc9634999ba4fb633eee554f84d8e8552af34
Imtaek Hong <imtaekh@gmail.com>
2015-08-18 10:56:24 -0700

    PROJECT_2_ ->PROJECT_3_

--- config/twitter.js
+++ config/twitter.js
@@ -1,8 +1,8 @@
 var Twit = require('twit');
 
 twitter = new Twit({
-  consumer_key    : process.env.WDI_PROJECT_2_TWITTER_CONSUMER_KEY,
-  consumer_secret : process.env.WDI_PROJECT_2_TWITTER_CONSUMER_SECRET,
+  consumer_key    : process.env.WDI_PROJECT_3_TWITTER_CONSUMER_KEY,
+  consumer_secret : process.env.WDI_PROJECT_3_TWITTER_CONSUMER_SECRET,
   app_only_auth   : true
 });
 
