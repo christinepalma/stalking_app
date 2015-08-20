0fa1fe32a15614c77a69ebc8ee132c0357ecc6bf
Simon Cho <simon.cho2093@gmail.com>
2015-08-20 14:19:13 -0700

    finished weather api

--- routes/weather.js
+++ routes/weather.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get("/:id",function (req,res) {
   var cityname=req.params.id
 
-  getJSON('http://api.openweathermap.org/data/2.5/weather?q='+cityname
+  getJSON('http://api.openweathermap.org/data/2.5/weather?q='+cityname+'&units=imperial'
   , function(data) {
    // do something with 'data'
    res.render('panels/weather',{data:data})
