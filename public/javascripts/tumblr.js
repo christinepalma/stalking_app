363f61a2bd8b6c5107aa0d2cb61e27c8ed4396c3
Imtaek Hong <imtaekh@gmail.com>
2015-08-19 22:37:54 -0700

    tumblr raw panel

--- public/javascripts/tumblr.js
+++ public/javascripts/tumblr.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $("header").css("background", "url("+$("header").data("url")+") no-repeat center");
   $("header").css("background-size", "cover");
 
-  $(".tweet_time").each(function(){
+  $(".feed_time").each(function(){
     var date = new Date($(this).data("time"));
     $(this).html(((date.getHours().toString().length==1)?("0"+date.getHours()):(date.getHours()))+":"
                 +((date.getMinutes().toString().length==1)?("0"+date.getMinutes()):(date.getMinutes()))+" "
@@ -14,13 +14,4 @@ $(document).ready(function(){
                 +date.getFullYear());
   });
 
-  $(".tweet_content").each(function(){
-    var url_array = $(this).data("urls").split(',');
-    var content = $(this).html();
-    url_array.forEach(function (url) {
-      if(url) content = content.replace(url,"<a href='"+url+"'>"+url+"</a>");
-    });
-    console.log(content);
-    $(this).html(content);
-  });
 });
